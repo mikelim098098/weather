6f09d9005bb149b0db7ca174b2737959083b33c6
Mike <mikelim098098@gmail.com>
2015-09-18 13:11:22 -0700

    added new Gem and edited config file for deployment

--- weather/app/models/alert.rb
+++ weather/app/models/alert.rb
@@ -2,13 +2,13 @@ class Alert < ActiveRecord::Base
 	belongs_to :user
   
   validates :title, :city_name, :alert_time, presence: true
-  validate :correct_alert_time
-
-  def correct_alert_time
-    hours = ['05', '06', '07', '08', '09', '10', '11']
-    if hours.include?(self.alert_time.strftime("%I"))
-    else
-      errors.add(:alert_time, "must be specified between 5am to 12pm")
-    end
-  end
+  # validate :correct_alert_time
+  #
+  # def correct_alert_time
+  #   hours = ['05', '06', '07', '08', '09', '10', '11']
+  #   if hours.include?(self.alert_time.strftime("%I"))
+  #   else
+  #     errors.add(:alert_time, "must be specified between 5am to 12pm")
+  #   end
+  # end
 end
